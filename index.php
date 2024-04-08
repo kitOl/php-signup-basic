3a409a47e959743bd6dfaf6e21eddb68aa637048
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-08 05:04:44 +0300

    add basic files

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Signup</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <nav>
+    <div class="wrapper">
+      <a href="index.php">Blog</a>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="discover.php">About us</a></li>
+        <li><a href="blog.php">Find Blogs</a></li>
+        <li><a href="signup.php">Sign up</a></li>
+        <li><a href="login.php">Log in</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+  <div class="wrapper">
+    
+    <section class="index-intro">
+      <h1>This is An Introduction</h1>
+      <p>Here is an important paragraph that explains the purpose of the werbsite</p>
+    </section>
+
+    <section class="index-categories">
+
+    </section>
+
+  </div>
+</body>
+
+</html>
